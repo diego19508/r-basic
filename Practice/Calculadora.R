76bcdf1570c5d86b8ddc1c95a2db9572d095361a
Diego <diego19508@gmail.com>
2021-01-29 02:06:47 +0100

    Create Calculadora.R

---
+++ Practice/Calculadora.R
@@ -0,0 +1,89 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3*5
+
+2%/%3+5
+
+2^3+5
+
+2^-4
+
+725%/%7
+
+725%%7
+
+725 - 103*7
+
+
+NA
+
+2^50
+
+0/0
+
+2^(-15)
+
+# Calculadora básica - Funciones
+
+sqrt(9)
+
+exp(1)
+
+log(1000,10)
+
+log10(1000)
+
+# Calculadora basica - Combinatoria
+
+factorial(7)
+
+factorial(4)
+
+factorial(0)
+
+choose(5,3)
+
+choose(5,7)
+
+6^log(4,6)
+
+choose(3,1)
+
+choose(5,2)
+
+# Funciones trigonometricas
+# Radianes
+
+cos(pi)
+
+cos(60*pi/180)
+
+sin(60*pi/180)
+
+sinpi(1/2) # = sin (pi/2)
+
+tan(pi)
+
+tan(60*pi/180)
+
+tan(pi/2)
+
+asin(0.866054)
+
+asin(0.8660254) *180/pi # arc sin en grados
+
+asin(5) # arc sin x in -1 , 1
+
+acos(-8)
+
+
+
+
+
+
+
+
+
+
