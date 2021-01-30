d8d8ee969fb5be85fe6c4439b84bfdb2e6b121bb
Diego <diego19508@gmail.com>
2021-01-30 21:06:29 +0100

    Create Variables_funciones.R

---
+++ Practice/Variables_funciones.R
@@ -0,0 +1,38 @@
+# Variable, objeto que sirve para guardar datos
+
+#nombre_variable = valor
+
+(pi^2)/2
+
+x = (pi^2)/2
+
+x
+
+y <- cos(pi\4)
+
+sin(pi/4) + cos(pi/4) -> z
+
+z
+
+edad = 30
+
+nombre = 'Diego'
+
+HOLA = 1
+
+hola = 5
+
+# NO USAR ACENTOS NI LETRAS POCO COMUNES
+
+pi.4 = 4*pi
+
+pi.4
+
+x = 2
+
+x = x^2
+
+x = sqrt(x)
+
+# FUNCIONES
+
