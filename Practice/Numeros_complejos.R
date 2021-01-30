b119b3f4c88a807d7379511616336fd1ea68f673
Diego <diego19508@gmail.com>
2021-01-30 21:54:40 +0100

    Create Numeros_complejos.R

---
+++ Practice/Numeros_complejos.R
@@ -0,0 +1,54 @@
+(3+2i)
+
+(3+2i)*5
+
+(3+2i)*(-1+3i)
+
+(3+2i)/(-1+3i)
+
+pi + sqrt(2i)
+
+z1 = complex(real = pi, imaginary = sqrt(2)) 
+z1
+
+
+sqrt(as.complex(-5))
+
+# La raiz cuadrada devuelve, de las dos soluciones la de
+# Re(z)>0, para obtener la otra, hay que multiplicar por -1 
+
+qrt(3+2i)
+exp(3+2i)
+sin(3+2i)
+cos(3+2i)
+
+#Modulo = sqrt(Re(z)^2 + Im(z)^2)
+
+Mod(z1)
+
+#Argumento = arctan(Im(z)/Re(z))
+# = arccos(Re(z)/Mod(z))
+# = arcsin(Im(z)/Mod(z))
+
+Arg(-1+0i)
+
+Arg(-1-2i)
+
+# Conjugado = Re(z) - Im(z)i
+
+Conj(z1)
+
+# Parte Real y Parte Imaginaria
+
+Re(z1)
+
+### z = Mod(z) * (cos(Arg(z))) + sin(Arg(z)i)
+
+z2 = complex(modulus = 2, argument = pi/2)
+
+z2
+
+Mod(z2)
+Arg(z2)
+
+pi/2
