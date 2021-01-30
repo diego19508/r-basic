1089dd918e8e75d66572e85bb4f04ce7a8b7d439
Diego <diego19508@gmail.com>
2021-01-30 21:31:54 +0100

    Create Ejercicio.R

---
+++ Practice/Ejercicio.R
@@ -0,0 +1,17 @@
+opBasic = function(a,b){
+  print('Suma')
+  print(a+b)
+  print('Resta')
+  print(paste(sprint('%i - %i',a,b)a-b))
+  print(paste(sprint('%i - %i',a,b)b-a))
+  print('Producto')
+  print(a*b)
+  print('Cociente de la division entera')
+  print(a%/%b)
+  print(b%/%a)
+  print('Resto de la division entera')
+  print(a%%b)
+  print(b%%a)
+}
+
+opBasic(6,4)
